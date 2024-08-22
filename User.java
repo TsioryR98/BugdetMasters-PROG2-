c10dca3402b0553878fd4779ff4eaceca8671a83
TsioryR98 <hei.tsiory.2@gmail.com>
2024-08-23 00:27:24 +0200

    feat: add new method for getting Top 3 expense categories

--- User.java
+++ User.java
@@ -1,11 +1,8 @@
 package BudgetMaster;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class User {
@@ -48,17 +45,75 @@ public class User {
         }
         return sum;
     }
+
     public double getRemainingBudget(double budgetMensuel){
         return budgetMensuel-getTotalSpentThisMonth() ;
     }
 
+    public String getTopCategories(){
+        /*
+        Map<Double, CategorieDepense> depenseParCategories = new HashMap<>();
+        for (Depense depenseEnCour : listeDepense) {
+            depenseParCategories.put(depenseEnCour.getMontant(),depenseEnCour.getCategorieDepense());
+        }
+
+        Map<CategorieDepense, Double> totalParCategories = new HashMap<>();
+        for (Map.Entry<Double, CategorieDepense> doubleCategorieDepenseEntry :
+                depenseParCategories.entrySet()) {
+            CategorieDepense categorieDepenses = doubleCategorieDepenseEntry.getValue();
+            Double montantDepense = doubleCategorieDepenseEntry.getKey();
+            totalParCategories.put(categorieDepenses,totalParCategories
+                    .getOrDefault(categorieDepenses,0.0)+montantDepense);
+        }
+        for (Map.Entry<CategorieDepense, Double> categorieDepenseDoubleEntry :
+                totalParCategories.entrySet()) {
+            System.out.println("categorie de depense : " + categorieDepenseDoubleEntry.getKey()
+                                + " total :" + categorieDepenseDoubleEntry.getValue()
+            );
+        }
+        */
+        Map<CategorieDepense, Double> totalParCategories = new HashMap<>();
+        for (Depense depenseEnCour : listeDepense) {
+            totalParCategories.put(depenseEnCour.getCategorieDepense(),depenseEnCour.getMontant());
+        }
+        //mettre Map dans List
+        List<Map.Entry<CategorieDepense,Double>> topCategorie = totalParCategories.entrySet().stream()
+                .sorted(Map.Entry.<CategorieDepense, Double>comparingByValue().reversed())
+                .limit(3)
+                .toList();
+        for (Map.Entry<CategorieDepense, Double> categorieDepenseDoubleEntry : topCategorie) {
+            System.out.println("Categorie: " +categorieDepenseDoubleEntry.getKey() +
+                                " /Total : " + categorieDepenseDoubleEntry.getValue()
+            );
+        }
+        return topCategorie.stream()
+                .map(categorieDepenseDoubleEntry -> categorieDepenseDoubleEntry.getKey().toString() +": "+
+                        categorieDepenseDoubleEntry.getValue().toString())
+                .collect(Collectors.joining(" "));
+    }
+
 
     public static void main(String[] args) {
-        User user1 = new User("max",12000.00);
-        Depense depense = new Depense("sakafo",4500.00,CategorieDepense.NOURRITURE, LocalDateTime.now());
+        User user1 = new User("max",240000.00);
+        Depense depense = new Depense("Nourriture",4500.00,CategorieDepense.NOURRITURE, LocalDateTime.now());
+        Depense depense1 = new Depense("Frais",6000.00,CategorieDepense.TRANSPORT, LocalDateTime.now());
+        Depense depense2 = new Depense("Autre",1500.00,CategorieDepense.AUTRES, LocalDateTime.now());
+        Depense depense3 = new Depense("Nourriture",1800.00,CategorieDepense.NOURRITURE, LocalDateTime.now());
+        Depense depense4 = new Depense("divertissement",2400.00,CategorieDepense.DIVERTISSEMENT, LocalDateTime.now());
+        Depense depense5 = new Depense("Frais",1200.00,CategorieDepense.TRANSPORT, LocalDateTime.now());
+        Depense depense6 = new Depense("divertissement",3100.00,CategorieDepense.DIVERTISSEMENT, LocalDateTime.now());
+
         user1.addExpense(depense);
+        user1.addExpense(depense1);
+        user1.addExpense(depense2);
+        user1.addExpense(depense3);
+        user1.addExpense(depense4);
+        user1.addExpense(depense5);
+        user1.addExpense(depense6);
+
         System.out.println(user1.getTotalSpentThisMonth());
-        System.out.println(user1.getRemainingBudget(12000.00));
+        System.out.println(user1.getRemainingBudget(24000.00));
+        System.out.println(user1.getTopCategories());
     }
 }
 
